5637e76ab9625915980c214222be4d01b70d1738
Wesley Leggo-Morrell <wes-lm@outlook.com>
2025-07-27 09:54:32 +0100

    Rebuild Wolfram

--- src/WolframModule.cpp
+++ src/WolframModule.cpp
@@ -1,5 +1,5 @@
-// Rack API documentation https://vcvrack.com/docs-v2/
-
+// https://vcvrack.com/docs-v2/
+// Make with: export RACK_DIR=/home/wes-l/Rack-SDK
 #include "plugin.hpp"
 
 
@@ -9,7 +9,7 @@ struct WolframModule : Module {
 		PARAMS_LEN
 	};
 	enum InputId {
-		PITCH_INPUT,
+		CLK_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
@@ -21,14 +21,43 @@ struct WolframModule : Module {
 		LIGHTS_LEN
 	};
 
+	float clockFreq = 1.f;
+	float blinkPhase = 0.f;
+	dsp::Timer clockTimer;
+	dsp::SchmittTrigger clockTrigger;
+
 	WolframModule() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(PITCH_PARAM, 0.f, 10.f, 5.f, "");
-		configInput(PITCH_INPUT, "");
+		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+		configInput(CLK_INPUT, "Clock");
 		configOutput(SINE_OUTPUT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
+		// Look how SEQ3 does it
+		// Clock
+		if (inputs[CLK_INPUT].isConnected()) {
+			clockTimer.process(args.sampleTime);
+
+			if (clockTrigger.process(inputs[CLK_INPUT].getVoltage(), 0.1f, 2.f)) {
+				float clockFreq = 1.f / clockTimer.getTime();
+				clockTimer.reset();
+				if (0.001f <= clockFreq && clockFreq <= 1000.f) {
+					this->clockFreq = clockFreq;
+				}
+			}
+		}
+		else {
+			// Default frequency when clock is unpatched
+			clockFreq = 2.f;
+		}
+
+		// Blink light at Clock Hz
+		// Accumulate the phase
+		blinkPhase += clockFreq * args.sampleTime;
+		if (blinkPhase >= 1.f)
+			blinkPhase -= 1.f;
+		lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
 	}
 };
 
@@ -36,7 +65,7 @@ struct WolframModule : Module {
 struct WolframModuleWidget : ModuleWidget {
 	WolframModuleWidget(WolframModule* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/MyModule.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/WolframModule.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
@@ -45,7 +74,7 @@ struct WolframModuleWidget : ModuleWidget {
 
 		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, WolframModule::PITCH_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, WolframModule::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, WolframModule::CLK_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, WolframModule::SINE_OUTPUT));
 
